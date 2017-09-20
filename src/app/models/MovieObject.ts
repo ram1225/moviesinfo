006720f4d6a25b039ba4daf7c1f7935df2ed535e
ram1225 <erroju.ram@gmail.com>
2017-09-20 12:16:24 +0530

    changed model class

--- src/app/models/MovieObject.ts
+++ src/app/models/MovieObject.ts
@@ -1,16 +1,8 @@
 import { Result } from './../models/Result';
 
 export class MovieObject {
-    page: number;
-    total_results: number;
-    total_pages: number;
-    results: Result[];
-
-
-    constructor(page: number, total_results: number, total_pages: number, results: Result[]) {
-        this.page = page;
-        this.total_results = total_results;
-        this.total_pages = total_pages;
-        this.results = results;
+  
+    constructor(public page: number,public total_results: number, public total_pages: number, public results: Result[]) {
+      
     }
 }
