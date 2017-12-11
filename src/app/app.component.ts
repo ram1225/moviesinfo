1a653b0502ec0b0b1fb466e4ab4e5def360d58a7
ram1225 <geek.emp25@gmail.com>
2017-12-11 23:23:46 +0530

    added dev, prod environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 // import { Router } from '@angular/router';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  backgroundColor = environment.headerBackgroundColor;
   // constructor(router: Router){
 
   // }
